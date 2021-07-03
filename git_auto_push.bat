f18da2f6613003503509469195e9689373eeaa10
spspider <spspider95@gmail.com>
2021-07-03 22:11:50 +0300

    removed bash file

--- git_auto_push.bat
+++ git_auto_push.bat
@@ -2,11 +2,8 @@ cd /d
 cd "/d/My Documents/Programming/Projects_Arduino/"
 git init
 git add .
-git commit -m "upload"
+git commit -m "first commit"
+git branch -M main
 git remote add origin https://github.com/spspider/Arduino.git
-git pull --rebase
-git fetch origin
-git branch --set-upstream-to=origin/master master
-git pull --allow-unrelated-histories
-git push origin --all
+git push -u origin main
 
